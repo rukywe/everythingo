d5cdb27e3e0c3f0500636c61fbbcb6cf1830e7d8
Kelvin Anigboro <rukywe@me.com>
2023-03-26 12:25:12 +0100

    Add more assertions to tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,13 +1,16 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck of length of 20 but got %v", len(d))
+		t.Errorf("Expected deck length of 16 but got %v", len(d))
 	}
 
 	if d[0] != "Ace of Spades" {
@@ -17,5 +20,17 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card to be Clubs, but got %v", d[len(d)-1])
 
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
 	}
 }
